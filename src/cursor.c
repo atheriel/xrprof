9835ee63ae75484772b585440bce6753378a8d6f
Aaron Jacobs <atheriel@gmail.com>
2020-05-20 20:10:08 +0000

    Dumps the context on an unknown stack setup.
    [skip-ci]

--- src/cursor.c
+++ src/cursor.c
@@ -41,6 +41,139 @@ void xrprof_destroy(struct xrprof_cursor *cursor) {
 
 #define MAX_SYM_LEN 128
 
+int dump_sexp(struct xrprof_cursor *cursor, SEXP sexp, char *buff, size_t len) {
+  int ret;
+  switch (TYPEOF(sexp)) {
+  case 0:
+    ret = snprintf(buff, len, "<nil>");
+    return ret >= len ? -ret : 0;
+  case 3:
+    ret = snprintf(buff, len, "<closure>");
+    return ret >= len ? -ret : 0;
+  case 4:
+    ret = snprintf(buff, len, "<env>");
+    return ret >= len ? -ret : 0;
+  case 5:
+    ret = snprintf(buff, len, "<promise>");
+    return ret >= len ? -ret : 0;
+  case 1: {
+    char name[MAX_SYM_LEN];
+    ret = copy_char(cursor->pid, (void *) PRINTNAME(sexp), name, MAX_SYM_LEN);
+    if (ret < 0) {
+      ret = snprintf(buff, len, "<trunc>");
+    } else {
+      ret = snprintf(buff, len, "%s", name);
+    }
+    return ret >= len ? -ret : 0;
+  }
+  case 2:
+    /* fallthrough */
+  case 6: {
+    const char *type = TYPEOF(sexp) == 2 ? "pairlist" : "lang";
+    size_t sublen = len - 8; /* Space for "(,<nil>)". */
+    SEXPREC car, cdr;
+    char carbuff[sublen], cdrbuff[sublen];
+
+    ret = copy_sexp(cursor->pid, (void *) CAR(sexp), &car);
+    if (ret < 0) {
+      fprintf(stderr, "error: Unexpected R structure: current %s item has no CAR.\n", type);
+      return ret;
+    }
+    ret = dump_sexp(cursor, &car, carbuff, sublen);
+    if (ret >= sublen) {
+      ret = snprintf(buff, len, "<%s:trunc>", type);
+      return ret >= len ? -ret : 0;
+    }
+
+    ret = copy_sexp(cursor->pid, (void *) CDR(sexp), &cdr);
+    if (ret < 0) {
+      fprintf(stderr, "error: Unexpected R structure: current %s item has no CDR.\n", type);
+      return ret;
+    }
+    ret = dump_sexp(cursor, &cdr, cdrbuff, sublen);
+    if (ret >= sublen) {
+      ret = snprintf(buff, len, "<%s:trunc>", type);
+      return ret >= len ? -ret : 0;
+    }
+
+    ret = snprintf(buff, len, "(%s, %s)", carbuff, cdrbuff);
+    return ret >= len ? -ret : 0;
+  }
+  default:
+    ret = snprintf(buff, len, "<sexptype:%d>", TYPEOF(sexp));
+    return ret < len ? -ret : 0;
+  }
+}
+
+int dump_context(struct xrprof_cursor *cursor, char *buff, size_t len) {
+  SEXPREC promargs, callfun, sysparent, call, cloenv;
+  int ret;
+  ret = copy_sexp(cursor->pid, (void *) cursor->cptr->promargs, &promargs);
+  if (ret < 0) {
+    fprintf(stderr, "error: Could not read SEXP for current promargs.\n");
+    return ret;
+  }
+  ret = copy_sexp(cursor->pid, (void *) cursor->cptr->callfun, &callfun);
+  if (ret < 0) {
+    fprintf(stderr, "error: Could not read SEXP for current callfun.\n");
+    return ret;
+  }
+  ret = copy_sexp(cursor->pid, (void *) cursor->cptr->sysparent, &sysparent);
+  if (ret < 0) {
+    fprintf(stderr, "error: Could not read SEXP for current sysparent.\n");
+    return ret;
+  }
+  ret = copy_sexp(cursor->pid, (void *) cursor->cptr->call, &call);
+  if (ret < 0) {
+    fprintf(stderr, "error: Could not read SEXP for current call.\n");
+    return ret;
+  }
+  ret = copy_sexp(cursor->pid, (void *) cursor->cptr->cloenv, &cloenv);
+  if (ret < 0) {
+    fprintf(stderr, "error: Could not read SEXP for current cloenv.\n");
+    return ret;
+  }
+
+  const char *fmt = "\
+context {\n\
+  nextcontext: %p\n\
+  callflag: %d\n\
+  cstacktop: %d\n\
+  evaldepth: %d\n\
+  promargs: %p %s\n\
+  callfun: %p %s\n\
+  sysparent: %p %s\n\
+  call: %p %s\n\
+  cloenv: %p %s\n\
+  intsusp: %d\n\
+  gcenabled: %d\n\
+  bcintactive: %d\n\
+}";
+
+  char dbuff[5][128];
+  dump_sexp(cursor, &promargs, dbuff[0], 128);
+  dump_sexp(cursor, &callfun, dbuff[1], 128);
+  dump_sexp(cursor, &sysparent, dbuff[2], 128);
+  dump_sexp(cursor, &call, dbuff[3], 128);
+  dump_sexp(cursor, &cloenv, dbuff[4], 128);
+
+  snprintf(buff, len, fmt,
+           (void *) cursor->cptr->nextcontext,
+           cursor->cptr->callflag,
+           cursor->cptr->cstacktop,
+           cursor->cptr->evaldepth,
+           (void *) cursor->cptr->promargs, dbuff[0],
+           (void *) cursor->cptr->callfun, dbuff[1],
+           (void *) cursor->cptr->sysparent, dbuff[2],
+           (void *) cursor->cptr->call, dbuff[3],
+           (void *) cursor->cptr->cloenv, dbuff[4],
+           cursor->cptr->intsusp,
+           cursor->cptr->gcenabled,
+           cursor->cptr->bcintactive);
+
+  return 0;
+}
+
 int xrprof_get_fun_name(struct xrprof_cursor *cursor, char *buff, size_t len) {
   SEXPREC call, fun, cdr, lhs, rhs;
   char lname[MAX_SYM_LEN], rname[MAX_SYM_LEN];
@@ -102,8 +235,9 @@ int xrprof_get_fun_name(struct xrprof_cursor *cursor, char *buff, size_t len) {
       written = snprintf(buff, len, "<Anonymous>");
     }
   } else {
-    /* fprintf(stderr, "TYPEOF(call)=%d; callflag=%d\n", TYPEOF(call), */
-    /*         cptr->callflag); */
+    char dbuff[512];
+    dump_context(cursor, dbuff, 512);
+    fprintf(stderr, "info: unknown %s\n", dbuff);
     written = snprintf(buff, len, "<Unknown>");
   }
 
